83c9ddd8b4420cfe612d5cb13036e05bea652485
wooooozin <wooooozin@gmail.com>
2023-05-04 22:31:52 +0900

    예외 처리 1

---
+++ StudyLog/src/day003/exception/ExceptionHandling1.java
@@ -0,0 +1,16 @@
+package day003.exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExceptionHandling1 {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("a.txt");
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        System.out.println("여기도 호출이 됩니다.");
+    }
+}
