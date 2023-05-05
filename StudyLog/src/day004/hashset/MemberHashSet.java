31251ade4dcd2372c733d57e80f32083506c9c62
wooooozin <wooooozin@gmail.com>
2023-05-06 01:04:04 +0900

    HashSet 구현

---
+++ StudyLog/src/day004/hashset/MemberHashSet.java
@@ -0,0 +1,39 @@
+package day004.hashset;
+
+import day004.collection.Member;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class MemberHashSet {
+    private HashSet<Member> hashSet;
+
+    public MemberHashSet() {
+        this.hashSet = new HashSet<Member>();
+    }
+
+    public void addMember(Member member) {
+        hashSet.add(member);
+    }
+
+    public boolean removeMember(int memberId) {
+        Iterator<Member> ir = hashSet.iterator();
+        Member member = ir.next(); // 다음 변수 반환, Iterator 순환
+        while (ir.hasNext()) {
+            int tempId = member.getMemberId();
+            if (tempId == memberId) {
+                hashSet.remove(member);
+                return true;
+            }
+        }
+        System.out.println(memberId + "is not founded");
+        return false;
+    }
+
+    public void showAllMember() {
+        for (Member member : hashSet) {
+            System.out.println(member);
+        }
+        System.out.println();
+    }
+}
