691733e5bf06c554cab5180486dafbfb685a8e35
wooooozin <wooooozin@gmail.com>
2023-05-09 22:20:58 +0900

    코딩테스트 2

---
+++ StudyLog/src/day007/ProgrammersQueue.java
@@ -0,0 +1,48 @@
+package day007;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ProgrammersQueue {
+    public static void main(String[] args) {
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+
+        System.out.println(Arrays.toString(solution(progresses, speeds)));
+    }
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            if ((100 - progresses[i]) % speeds[i] == 0) {
+                q.add((100 - progresses[i]) / speeds[i]);
+            } else {
+                q.add((100 - progresses[i]) / speeds[i] + 1);
+            }
+        }
+
+        int x = q.poll();
+        int count = 1;
+        while (!q.isEmpty()) {
+            if (x >= q.peek()) {
+                count++;
+                q.poll();
+            } else {
+                list.add(count);
+                count = 1;
+                x = q.poll();
+            }
+        }
+        list.add(count);
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
