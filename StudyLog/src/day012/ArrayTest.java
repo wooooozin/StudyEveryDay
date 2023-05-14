a36518a589b49c7c71bba0f4285b7d92fc8a7460
wooooozin <wooooozin@gmail.com>
2023-05-15 00:14:32 +0900

    ArrayList 정리

--- StudyLog/src/day012/ArrayTest.java
+++ StudyLog/src/day012/ArrayTest.java
@@ -1,5 +1,6 @@
 package day012;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 // Array, ArrayList 복습
@@ -28,5 +29,55 @@ public class ArrayTest {
 
         // Arrays.toString() 메서드 사용해 출력
         System.out.println(Arrays.toString(numbers)); // [100, 95, 80, 70, 60]
+
+        // ArrayList
+        // 생성자
+        ArrayList<Integer> intArrayList = new ArrayList<>(); // 기본 크기가 10인 배열 생성
+        System.out.println(intArrayList.size()); // 0
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        // add, add(index, value)
+        stringArrayList.add("Mike");
+        stringArrayList.add(0, "James");
+        System.out.println(stringArrayList); // [James, Mike]
+
+        // get 해당 인덱스 값 반환
+        String str = stringArrayList.get(0);
+        String str1 = stringArrayList.get(1);
+        System.out.println(str); // James
+        System.out.println(str1); // Mike
+
+        // set 인덱스로 값 변경
+        stringArrayList.set(0, "Kiki");
+        System.out.println(stringArrayList); // [Kiki, Mike]
+
+        // indexOf 값을 제공하면 해당 값의 첫번째 인덱스를 반환
+        stringArrayList.add("AA");
+        stringArrayList.add("BB");
+        stringArrayList.add("AA");
+        System.out.println(stringArrayList); // [Kiki, Mike, AA, BB, AA]
+        System.out.println(stringArrayList.indexOf("AA")); // 2
+
+        // lastIndexOf 해당 값의 마지막 인덱스 반환
+        System.out.println(stringArrayList.lastIndexOf("Kiki")); // 0
+        System.out.println(stringArrayList.lastIndexOf("AA")); // 4
+
+        // remove(index), remove(value) 해당 인덱스 삭제, 해당 데이터 중 첫번째 값 삭제
+        stringArrayList.remove(0);
+        System.out.println(stringArrayList); // [Mike, AA, BB, AA]
+        stringArrayList.remove("AA");
+        System.out.println(stringArrayList); // // [Mike, BB, AA]
+
+        // contains(value) 값을 포함하는지 boolean으로 반환
+        System.out.println(stringArrayList.contains("BB")); // true
+        System.out.println(stringArrayList.contains("CC")); // false
+
+        // clear() 모두 삭제, isEmpty 배열 비었는지 확인
+        stringArrayList.clear();
+        System.out.println(stringArrayList.isEmpty()); // true
+
+        // size() 요소 개수 반환
+        System.out.println(stringArrayList.size()); // 0
+
     }
 }
