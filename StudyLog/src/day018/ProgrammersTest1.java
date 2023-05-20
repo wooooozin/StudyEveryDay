07fcade12f5c4afed9e886d049e701689730f0d2
wooooozin <wooooozin@gmail.com>
2023-05-20 19:24:58 +0900

    프로그래머스 문제 풀기

--- StudyLog/src/day018/ProgrammersTest1.java
+++ StudyLog/src/day018/ProgrammersTest1.java
@@ -1,7 +1,30 @@
 package day018;
 
-public class ProgrammersTest1 {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.HashMap;
 
+public class ProgrammersTest1 {
+    public static String solution(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char key : map.keySet()) {
+            if (map.get(key) == 1) {
+                sb.append(key);
+            }
+        }
+        System.out.println(map);
+        String result = sb.toString();
+        char[] answer = result.toCharArray();
+        Arrays.sort(answer);
+        return String.valueOf(answer);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("abcabcadc"));
+        System.out.println(solution("abdc"));
+        System.out.println(solution("hello"));
     }
 }
