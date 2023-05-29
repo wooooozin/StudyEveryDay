da9add2d5194ea8925344c3ceb92526da53d8223
wooooozin <wooooozin@gmail.com>
2023-05-29 23:01:52 +0900

    백준 문제 풀기

---
+++ StudyLog/src/day027/Palindrome1254.java
@@ -0,0 +1,45 @@
+package day027;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Palindrome1254 {
+    public static boolean checkPalindrome(String s) {
+        Deque<String> deque = new ArrayDeque();
+        boolean isPalindrome = true;
+
+        for (String str : s.split("")) {
+            deque.addFirst(str);
+        }
+
+        while (!deque.isEmpty()) {
+            String s1 = deque.pollFirst();
+            String s2 = deque.pollLast();
+
+            if (s1 != null && s2 != null && !s1.equals(s2)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        return isPalindrome;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        StringBuilder sb = new StringBuilder(input);
+        int length = input.length();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (checkPalindrome(input.substring(i))) {
+                System.out.println(input.substring(i));
+                break;
+            }
+            length++;
+            sb.append(input.charAt(i));
+        }
+        System.out.println(length);
+        System.out.println(sb.toString());
+    }
+}
