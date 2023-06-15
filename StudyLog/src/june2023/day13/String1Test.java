4b170ce9a54bb42f63a729bec2773f2e20b2d1cb
wooooozin <wooooozin@gmail.com>
2023-06-15 21:14:08 +0900

    회문 문자열 수정

--- StudyLog/src/june2023/day13/String1Test.java
+++ StudyLog/src/june2023/day13/String1Test.java
@@ -4,25 +4,18 @@ import java.util.Scanner;
 
 public class String1Test {
 
-    public static boolean solution(String str) {
-        str = str.toLowerCase();
-        int left = 0;
-        int right = str.length() - 1;
-        while (left < right) {
-            if (str.charAt(left) == str.charAt(right)) {
-                left++;
-                right--;
-                continue;
-            } else {
-                return false;
-            }
+    public static String solution(String str) {
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(tmp)) {
+            return answer;
         }
-        return true;
+
+        return answer;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-        boolean isTrue = solution(input);
-        System.out.println(isTrue ? "YES" : "NO");
+        System.out.println(solution(input));
     }
 }
