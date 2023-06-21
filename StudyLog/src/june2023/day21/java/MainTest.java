7d213e8eebe3e1f8e5f4b73d3df64b4a53cbe32c
wooooozin <wooooozin@gmail.com>
2023-06-21 23:27:08 +0900

    코테 정답 비교

--- StudyLog/src/june2023/day21/java/MainTest.java
+++ StudyLog/src/june2023/day21/java/MainTest.java
@@ -1,5 +1,7 @@
 package june2023.day21.java;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainTest {
@@ -25,12 +27,52 @@ public class MainTest {
         }
         sb.setLength(0);
     }
+
+    public static boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static ArrayList<Integer> solution1(int n, int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp /= 10;
+            }
+            if (isPrime(res)) {
+                list.add(res);
+            }
+        }
+        return list;
+    }
+
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+//        for (int i = 0; i < n; i++) {
+//            solution(sc.nextInt());
+//        }
+
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            solution(sc.nextInt());
+            arr[i] = sc.nextInt();
+        }
+        for (int x : solution1(n, arr)) {
+            System.out.print(x +  " ");
         }
     }
 }
