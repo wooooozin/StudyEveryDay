125676ac26e16bfd842fed7d2f020b72db3729df
wooooozin <wooooozin@gmail.com>
2023-07-19 07:32:54 +0900

    스택관련 문제 풀이 - 레이저

---
+++ StudyLog/src/july2023/day19/Main.java
@@ -0,0 +1,35 @@
+package july2023.day19;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static int solution(String str) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                stack.push(str.charAt(i));
+            } else {
+                stack.pop();
+                if (str.charAt(i - 1) == '(') {
+                    answer += stack.size();
+                } else {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(solution(str));
+
+    }
+}
