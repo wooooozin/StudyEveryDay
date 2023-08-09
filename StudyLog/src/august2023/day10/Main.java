5f45bab3cb7df5d7f7a56be8082c9551bf97542f
wooooozin <wooooozin@gmail.com>
2023-08-10 07:27:33 +0900

    재귀함수 - 피보나치 수열 배열에 담아서 반환하기

--- StudyLog/src/august2023/day10/Main.java
+++ StudyLog/src/august2023/day10/Main.java
@@ -1,20 +1,23 @@
 package august2023.day10;
 
 public class Main {
+    static int[] arr;
     private static int dfs(int n) {
         if (n == 1) {
-            return 1;
+            return arr[n] = 1;
         } else if (n == 2) {
-            return 1;
+            return arr[n] = 1;
         } else {
-            return dfs(n - 2) + dfs(n - 1);
+            return arr[n] = dfs(n - 2) + dfs(n - 1);
         }
     }
 
     public static void main(String[] args) {
-        int n = 10;
+        int n = 20;
+        arr = new int[n + 1];
+        dfs(n);
         for (int i = 1; i <= n; i++) {
-            System.out.print(dfs(i) + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 }
